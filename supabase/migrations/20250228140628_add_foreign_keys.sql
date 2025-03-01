efa9d90d8525446efb6ed831604c1d86bcf8e244
sisqodataclub <103331558+sisqodataclub@users.noreply.github.com>
2025-03-01 01:02:39 +0000

    Delete supabase/migrations/20250228140628_add_foreign_keys.sql

--- supabase/migrations/20250228140628_add_foreign_keys.sql
+++
@@ -1,9 +0,0 @@
-
--- Alter Table to Add Foreign Keys
-
-ALTER TABLE silver.listings_df_sil
-        ADD COLUMN IF NOT EXISTS "properties_df_sil_id" INTEGER;
-ALTER TABLE silver.listings_df_sil
-        ADD CONSTRAINT fk_properties_df_sil_id
-        FOREIGN KEY ("properties_df_sil_id") REFERENCES silver.properties_df_sil("id")
-        ON DELETE CASCADE;
